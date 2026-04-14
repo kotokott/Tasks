e36f67469580f20b1051c438e738ca0edd9c26da
kotokott <kraynovgr@gmail.com>
2026-04-14 19:41:24 +0300

    task8 added

---
+++ CodeRun/src/task8.java
@@ -0,0 +1,46 @@
+/*
+Дан список. Определите, является ли он монотонно возрастающим
+(то есть верно ли, что каждый элемент этого списка строго больше предыдущего).
+
+Выведите YES, если массив монотонно возрастает и NO в противном случае.
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        /*
+        Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+        int n = Integer.parseInt(reader.readLine());
+        writer.write(String.valueOf(n));
+        */
+        Integer lastNum = null;
+        String[] input = reader.readLine().split(" ");
+        for (String numStr : input) {
+            int num = Integer.parseInt(numStr);
+            if (lastNum == null) {
+                lastNum = num;
+                continue;
+            }
+
+            if (num <= lastNum) {
+                writer.write("NO");
+                reader.close();
+                writer.close();
+                return;
+            }
+            lastNum = num;
+        }
+        writer.write("YES");
+
+        reader.close();
+        writer.close();
+    }
+}
